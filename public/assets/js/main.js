8ab631902ca6b61e1817671955bbccc8a83d6999
Jonny Andreola <jonnyandreola@gmail.com>
2014-05-02 19:43:22 +0000

    included content on all pages

---
+++ public/assets/js/main.js
@@ -0,0 +1,10 @@
+$(window).on('scroll resize', function(){
+    var height = $(window).scrollTop();
+    var width = $(window).width();
+    if(height > 40 && width <= 560){
+        $('body > header').css({'position': 'fixed', 'top': '-40px'});
+        $('body > main').css('padding-top', '80px');
+    } else {
+        $('body > header, body > main').removeAttr('style');
+    }
+})
